e3f5768d2094fd1eea599a0d596056ebfee7e640
Nelfiguera <nerzonfiguera01@gmail.com>
2023-05-23 22:53:45 -0300

    commit inicial

---
+++ js/index.js
@@ -0,0 +1,35 @@
+/*Aquí declaramos las variables e introducimos los elementos que vamos a utilizar para añadir interactividad a la página, las variables las creamos con nombres semejante a los nombres de los elementos. Estos elementos los tomamos del archivo HTML (index.html).*/
+
+const displayValorAnterior = document.getElementById("valor-anterior");
+const displayValorActual = document.getElementById("valor-actual");
+const botonesNumeros = document.querySelectorAll(".numero");
+const botonesOperadores = document.querySelectorAll(".operador");
+
+const display = new Display(displayValorAnterior, displayValorActual);
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener("click", () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener("click", () => display.computar(boton.value))
+});
+
+
+
+
+/*const calculadora = new Calculadora();
+
+console.log(calculadora.sumar(2,3))
+console.log(calculadora.restar(2,3))
+console.log(calculadora.dividir(2,3))
+console.log(calculadora.multiplicar(2,3))*/
+
+
+
+
+
+
+
+
+
